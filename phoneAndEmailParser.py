6e759ee156a3b4ee84d46f2e4038e3f0004921ee
harshitgarg22 <hg1229@gmail.com>
2019-08-20 21:13:57 +0530

    Add comments and cleaned code to make it pretty

--- phoneAndEmailParser.py
+++ phoneAndEmailParser.py
@@ -6,12 +6,12 @@ import pyperclip
 # Define template for phone number and email address separately
 
 phoneNumberRegex = re.compile(r'''(
-(\d{3}|\(\d\))?                       # area code as 400 or (400). Is optional.
-(\s|-|\.)?                            # Separator. As \s character, -, . . Is optional.
-(\d{3})                                 # first 3 digits
-(\s|-|\.)                             # Separator. As \s character, -, . .
-(\d{4})                                 # last 4 digits
-(\s*(ext|x|ext.)\s*(\d{2,5}))?        # Extension
+(\d{3}|\(\d\))?                      # area code as 400 or (400). Is optional.
+(\s|-|\.)?                           # Separator. As \s character, -, . . Is optional.
+(\d{3})                              # first 3 digits
+(\s|-|\.)                            # Separator. As \s character, -, . .
+(\d{4})                              # last 4 digits
+(\s*(ext|x|ext.)\s*(\d{2,5}))?       # Extension
 )''', re.VERBOSE)
 
 emailAddressRegex = re.compile(r'''(
@@ -34,6 +34,8 @@ emailMatchesRaw = emailAddressRegex.findall(text)
 phoneMatches = []
 emailMatches = []
 
+# Nicely format everything before pasting into clipboard
+
 for groups in phoneMatchesRaw:
     phoneNum = '-'.join([groups[1], groups[3], groups[5]])
     phoneMatches.append(phoneNum)
@@ -41,10 +43,11 @@ for groups in phoneMatchesRaw:
 for groups in emailMatchesRaw:
     emailMatches.append(groups)
 
-# test = (str('\n'.join([phoneMatches, emailMatches])))
+# Copy the str into clipboard
 
 pyperclip.copy(str('\n'.join(phoneMatches + emailMatches)))  # Concatenate the two lists instead of join([[],[]]), because that'd treat it as a list only.
 
+# Show what all was copied
 
 print("The following data was copied: \n")
 print(pyperclip.paste())
